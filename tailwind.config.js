6aff44cb3634b67de7cbe06327ca47921d52dd75
mecirmartin <mecir.martin@gmail.com>
2021-01-17 15:20:43 +0100

    updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enable: true,
-    content: ["./src/pages/*.tsx"],
+    content: ["./pages/*.tsx"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
